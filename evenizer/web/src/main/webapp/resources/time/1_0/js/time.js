711cd0c8be58e81843160b03e83c670629324fc4
bartgerard <bart.gerard@gmail.com>
2015-07-18 15:54:03 +0200

    TARDIS + time display

--- evenizer/web/src/main/webapp/resources/time/1_0/js/time.js
+++ evenizer/web/src/main/webapp/resources/time/1_0/js/time.js
@@ -28,6 +28,28 @@ $(document).ready(function () {
         $(this).hide();
     });
 
-    $("#slider").slider();
+    $(".button").click(function () {
+        $(".clock > .arrowLong").css("animation-direction", "reverse");
+        $(".clock > .arrowShort").css("animation-direction", "reverse");
+        $(".spiral").css("animation-play-state", "running")
+    });
+
+    /*$("#slider").slider();*/
+
 
 });
+
+var interval = setInterval(tick, 1000);
+
+function tick() {
+    var date = new Date();
+    date.getYear();
+    $(".year").text(date.getFullYear());
+    $(".month").text(months[date.getMonth()]);
+    $(".day").text(date.getDay());
+    $(".hours").text(date.getHours());
+    $(".minutes").text(date.getMinutes());
+    $(".seconds").text(date.getSeconds());
+}
+
+var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
